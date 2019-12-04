08b37eed0bdf3fcbb8510269d9c5b945af7f51e8
hugo Aparicio <hugo.aparicio8@me.com>
2019-12-04 09:28:22 -0700

    Checkpoint 3

---
+++ controllers/MenuController.js
@@ -0,0 +1,74 @@
+const inquirer = require('inquirer');
+
+ module.exports = class MenuController {
+   constructor(){
+    this.mainMenuQuestions = [
+        {
+         type: "list",
+          name: "mainMenuChoice",
+          message: "Please choose from an option below: ",
+          choices: [
+            "Add new contact",
+            "Get Date",
+            "Remind Me",
+            "Exit"
+          ]
+        }
+      ];
+      this.contacts = [];
+   }
+
+   main(){
+    console.log(`Welcome to AddressBloc!`);
+     inquirer.prompt(this.mainMenuQuestions).then((response) => {
+       switch(response.mainMenuChoice){
+         case "Add new contact":
+           this.addContact();
+           break;
+         case "Get Date":
+            this.getDate();
+            break;
+         case "Remind Me":
+            this.remindMe();
+            break;
+         case "Exit":
+           this.exit();
+         default:
+           console.log("Invalid input");
+           this.main();
+       }
+     })
+     .catch((err) => {
+       console.log(err);
+     });
+  }
+
+  clear(){
+    console.log("\x1Bc");
+  }
+  
+  addContact(){
+    this.clear();
+    console.log('addContact called');
+    this.main();
+  }
+
+  getDate(){
+    this.clear();
+    console.log(Date().toLocaleString());
+    this.main();
+  }
+
+  exit(){
+    console.log("Thanks for using AddressBloc!");
+    process.exit();
+  }
+  getContactCount(){
+    return this.contacts.length;
+  }
+  remindMe(){
+    console.log('Learning is a life-long pursuit');
+    return 'Learning is a life-long pursuit';
+  }
+
+ }
